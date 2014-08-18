fd693c796103056208f9d4a94bad68036ab0cbcb
Nauman Iftikhar <naumanpices88@gmail.com>
2014-08-19 00:32:57 +0500

    New Screens and Artwork 18Aug

--- DrawingCanvas.h
+++ DrawingCanvas.h
@@ -16,7 +16,30 @@
     UITouch *touch;
     CGPoint lastPoint;
     CGPoint currentPoint;
-
+    
+    
+    //=================== THE MIGHTY BOOL===========//
+    
+    BOOL DotImage1;
+    BOOL DotImage2;
+    BOOL DotImage3;
+    BOOL DotImage4;
+    BOOL DotImage5;
+    BOOL DotImage6;
+    BOOL DotImage7;
+    BOOL DotImage8;
+    BOOL DotImage9;
+    BOOL DotImage10;
+    BOOL DotImage11;
+    BOOL DotImage12;
+    BOOL DotImage13;
+    BOOL DotImage14;
+    BOOL DotImage15;
+    
+    //=================== THE MIGHTY BOOL===========//
+    
+    
+    //==================== DOTS====================//
     IBOutlet UIImageView *dotImage1;
     IBOutlet UIImageView *dotImage2;
     IBOutlet UIImageView *dotImage3;
@@ -33,6 +56,47 @@
     IBOutlet UIImageView *dotImage14;
     IBOutlet UIImageView *dotImage15;
     
+    //==================== DOTS====================//
+    
+    //==================== Horizontal Lines====================//
+    
+    IBOutlet UIImageView *HorizontalLine1;
+    IBOutlet UIImageView *HorizontalLine2;
+    IBOutlet UIImageView *HorizontalLine3;
+    IBOutlet UIImageView *HorizontalLine4;
+    IBOutlet UIImageView *HorizontalLine5;
+    IBOutlet UIImageView *HorizontalLine6;
+    IBOutlet UIImageView *HorizontalLine7;
+    IBOutlet UIImageView *HorizontalLine8;
+    IBOutlet UIImageView *HorizontalLine9;
+    IBOutlet UIImageView *HorizontalLine10;
+    IBOutlet UIImageView *HorizontalLine11;
+    IBOutlet UIImageView *HorizontalLine12;
+
+    //==================== Horizontal Lines====================//
+   
+    //==================== Vertical Lines====================//
+    IBOutlet UIImageView *VerticalLine1;
+    IBOutlet UIImageView *VerticalLine2;
+    IBOutlet UIImageView *VerticalLine3;
+    IBOutlet UIImageView *VerticalLine4;
+    IBOutlet UIImageView *VerticalLine5;
+    IBOutlet UIImageView *VerticalLine6;
+    IBOutlet UIImageView *VerticalLine7;
+    IBOutlet UIImageView *VerticalLine8;
+    IBOutlet UIImageView *VerticalLine9;
+    IBOutlet UIImageView *VerticalLine10;
+    
+    
+    //==================== Vertical Lines====================//
+    
+    //==================== Diagonal Lines====================//
+    
+    IBOutlet UIImageView *DiagonalLine1;
+    IBOutlet UIImageView *DiagonalLine2;
+    
+    
+    //==================== Diagonal Lines====================//
     
     IBOutlet UIButton *NextScreen;
     
@@ -40,5 +104,6 @@
     AVAudioPlayer * backgroundMusic;
 }
 @property(strong,nonatomic) MPMoviePlayerController * moviePlayer;
+- (IBAction)ResetButton:(id)sender;
 
 @end
--- DrawingCanvas.m
+++ DrawingCanvas.m
@@ -48,7 +48,7 @@
     
     
     
-    UIGraphicsBeginImageContext(drawImage.frame.size);
+ /*   UIGraphicsBeginImageContext(drawImage.frame.size);
     [drawImage.image drawInRect:CGRectMake(0, 0, drawImage.frame.size.width, drawImage.frame.size.height)];
     
     CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
@@ -60,7 +60,7 @@
     CGContextStrokePath(UIGraphicsGetCurrentContext());
     drawImage.image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    lastPoint = currentPoint;
+    lastPoint = currentPoint;   */
     
  /*   if ([touch view] == dotImage1 && [touch view] == dotImage2) {
         CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
@@ -82,7 +82,7 @@
         drawImage.image = nil;
     } */
     
-    if ([touch view] == dotImage1 && [touch view] == dotImage2 && [touch view] == dotImage3 && [touch view] == dotImage4 && [touch view] == dotImage5 && [touch view] == dotImage6 && [touch view] == dotImage7 && [touch view] == dotImage8 && [touch view] == dotImage9 && [touch view] == dotImage10 && [touch view] == dotImage11 && [touch view] == dotImage12 && [touch view] == dotImage13 && [touch view] == dotImage14 && [touch view] == dotImage15){
+ /*   if ([touch view] == dotImage1 && [touch view] == dotImage2 && [touch view] == dotImage3 && [touch view] == dotImage4 && [touch view] == dotImage5 && [touch view] == dotImage6 && [touch view] == dotImage7 && [touch view] == dotImage8 && [touch view] == dotImage9 && [touch view] == dotImage10 && [touch view] == dotImage11 && [touch view] == dotImage12 && [touch view] == dotImage13 && [touch view] == dotImage14 && [touch view] == dotImage15){
         
         
         CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
@@ -96,16 +96,231 @@
         UIGraphicsEndImageContext();
         lastPoint = currentPoint;
         
+    }   */
+    
+    
+    
+ /*   if (CGRectContainsRect(dotImage1.frame, dotImage2.frame) && CGRectContainsPoint(dotImage1.frame, currentPoint)) {
+    
+        [HorizontalLine1 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+        
     }
+    else if (CGRectContainsRect(dotImage2.frame, dotImage3.frame) && CGRectContainsPoint(dotImage2.frame, currentPoint)){
+    
+        [HorizontalLine2 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }  */
+    
     
     NSLog(@"%f %f",currentPoint.x,currentPoint.y);
     
-
+/*    if (CGRectContainsPoint(dotImage1.frame, currentPoint) || CGRectContainsPoint(dotImage2.frame, currentPoint)) {
+        
+        [HorizontalLine1 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    else if (CGRectContainsPoint(dotImage2.frame, currentPoint) || CGRectContainsPoint(dotImage3.frame, currentPoint)){
+      
+        [HorizontalLine2 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    else if (CGRectContainsPoint(dotImage3.frame, currentPoint) || CGRectContainsPoint(dotImage4.frame, currentPoint)){
+       
+        [HorizontalLine3 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    else if (CGRectContainsPoint(dotImage4.frame, currentPoint) || CGRectContainsPoint(dotImage5.frame, currentPoint)){
+       
+        [HorizontalLine4 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    else if (CGRectContainsPoint(dotImage5.frame, currentPoint) || CGRectContainsPoint(dotImage10.frame, currentPoint)){
+        
+        [VerticalLine3 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    else if (CGRectContainsPoint(dotImage10.frame, currentPoint) || CGRectContainsPoint(dotImage15.frame, currentPoint)){
+        
+        [VerticalLine4 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    else if (CGRectContainsPoint(dotImage1.frame, currentPoint) || CGRectContainsPoint(dotImage6.frame, currentPoint)){
+        
+        [VerticalLine1 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    else if (CGRectContainsPoint(dotImage6.frame, currentPoint) || CGRectContainsPoint(dotImage11.frame, currentPoint)){
+        
+        [VerticalLine2 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    else if (CGRectContainsPoint(dotImage15.frame, currentPoint) || CGRectContainsPoint(dotImage8.frame, currentPoint)){
+        
+        [DiagonalLine1 setImage:[UIImage imageNamed:@"right red diagonal line 18Aug.png"]];
+    }
+    else if (CGRectContainsPoint(dotImage8.frame, currentPoint) ||  CGRectContainsPoint(dotImage11.frame, currentPoint)){
+        
+        [DiagonalLine2 setImage:[UIImage imageNamed:@"left red diagonal line 18Aug.png"]];
+    } */
+    /*else if (CGRectContainsPoint(dotImage11.frame, currentPoint) || CGRectContainsPoint(dotImage3.frame, currentPoint)){
+        dotImage11.tag = 11;
+    }
+    else if (CGRectContainsPoint(dotImage12.frame, currentPoint) || CGRectContainsPoint(dotImage3.frame, currentPoint)){
+        dotImage12.tag = 12;
+    }
+    else if (CGRectContainsPoint(dotImage13.frame, currentPoint) || CGRectContainsPoint(dotImage3.frame, currentPoint)){
+        dotImage13.tag = 13;
+    }
+    else if (CGRectContainsPoint(dotImage14.frame, currentPoint) || CGRectContainsPoint(dotImage3.frame, currentPoint)){
+        dotImage14.tag = 14;
+    }
+    else if (CGRectContainsPoint(dotImage15.frame, currentPoint) || CGRectContainsPoint(dotImage3.frame, currentPoint)){
+        dotImage15.tag = 15;
+    } */
+    
+    if (CGRectContainsPoint(dotImage1.frame, currentPoint)){
+        
+        DotImage1 = YES;
+    }
+    if (CGRectContainsPoint(dotImage2.frame, currentPoint)){
+        
+        DotImage2 = YES;
+    }
+    if (CGRectContainsPoint(dotImage3.frame, currentPoint)){
+        
+        DotImage3 = YES;
+    }
+    if (CGRectContainsPoint(dotImage4.frame, currentPoint)){
+        
+        DotImage4 = YES;
+    }
+    if (CGRectContainsPoint(dotImage5.frame, currentPoint)){
+        
+        DotImage5 = YES;
+    }
+    if (CGRectContainsPoint(dotImage6.frame, currentPoint)){
+        
+        DotImage6 = YES;
+    }
+    if (CGRectContainsPoint(dotImage7.frame, currentPoint)){
+        
+        DotImage7 = YES;
+    }
+    if (CGRectContainsPoint(dotImage8.frame, currentPoint)){
+        
+        DotImage8 = YES;
+    }
+    if (CGRectContainsPoint(dotImage9.frame, currentPoint)){
+        
+        DotImage9 = YES;
+    }
+    if (CGRectContainsPoint(dotImage10.frame, currentPoint)){
+        
+        DotImage10 = YES;
+    }
+    if (CGRectContainsPoint(dotImage11.frame, currentPoint)){
+        
+        DotImage11 = YES;
+    }
+    if (CGRectContainsPoint(dotImage12.frame, currentPoint)){
+        
+        DotImage12 = YES;
+    }
+    if (CGRectContainsPoint(dotImage13.frame, currentPoint)){
+        
+        DotImage13 = YES;
+    }
+    if (CGRectContainsPoint(dotImage14.frame, currentPoint)){
+        
+        DotImage14 = YES;
+    }
+    if (CGRectContainsPoint(dotImage15.frame, currentPoint)){
+        
+        DotImage15 = YES;
+    }
+    
+    
+    
+    
+    
+    
+    if (DotImage1 && DotImage2) {
+        
+        [HorizontalLine1 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    if (DotImage2 && DotImage3){
+        
+        [HorizontalLine2 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    if (DotImage3 && DotImage4){
+        
+        [HorizontalLine3 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    if (DotImage4 && DotImage5){
+        
+        [HorizontalLine4 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    if (DotImage5 && DotImage10){
+        
+        [VerticalLine3 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    if (DotImage10 && DotImage15){
+        
+        [VerticalLine4 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    if (DotImage1 && DotImage6){
+        
+        [VerticalLine1 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    if (DotImage6 && DotImage11){
+        
+        [VerticalLine2 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    if (DotImage15 && DotImage8){
+        
+        [DiagonalLine1 setImage:[UIImage imageNamed:@"right red diagonal line 18Aug.png"]];
+    }
+    if (DotImage8 && DotImage11){
+        
+        [DiagonalLine2 setImage:[UIImage imageNamed:@"left red diagonal line 18Aug.png"]];
+    }
+    if (DotImage6 && DotImage7) {
+        [HorizontalLine9 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    if (DotImage7 && DotImage8) {
+        [HorizontalLine10 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    if (DotImage8 && DotImage9) {
+        [HorizontalLine11 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    if (DotImage9 && DotImage10) {
+        [HorizontalLine12 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    if (DotImage7 && DotImage8) {
+        [HorizontalLine10 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    if (DotImage2 && DotImage7) {
+        [VerticalLine5 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    if (DotImage7 && DotImage12) {
+        [VerticalLine6 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    if (DotImage3 && DotImage8) {
+        //[VerticalLine7 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    if (DotImage8 && DotImage13) {
+        [VerticalLine8 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    if (DotImage4 && DotImage9) {
+        [VerticalLine9 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    if (DotImage9 && DotImage14) {
+        [VerticalLine10 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    
+    
+    
+    
+    
+    
+    
+    
    
-    if (CGRectContainsPoint(dotImage1.frame, currentPoint)) {
+    if (CGRectContainsPoint(dotImage1.frame, currentPoint) ) {
         dotImage1.tag = 1;
     }
-    else if (CGRectContainsPoint(dotImage2.frame, currentPoint)){
+    else if (CGRectContainsPoint(dotImage2.frame, currentPoint) ){
         dotImage2.tag = 2;
     }
     else if (CGRectContainsPoint(dotImage3.frame, currentPoint)){
@@ -149,7 +364,9 @@
     }
     
     
+
 }
+
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
     
@@ -158,10 +375,59 @@
     NSLog(@"%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d ",dotImage1.tag,dotImage2.tag,dotImage3.tag,dotImage4.tag,dotImage5.tag,dotImage6.tag,dotImage7.tag,dotImage8.tag,dotImage9.tag,dotImage10.tag,dotImage11.tag, dotImage12.tag, dotImage13.tag, dotImage14.tag, dotImage15.tag);
     
     NSLog(@"%f, %f",dotImage1.frame.size.width, dotImage1.frame.size.height);
+    
+    
+    
+    if (dotImage1.tag != 1 || dotImage2.tag != 2 || dotImage3.tag != 3 || dotImage4.tag != 4 || dotImage5.tag != 5 || dotImage6.tag != 6 || dotImage8.tag != 8 || dotImage10.tag != 10 || dotImage11.tag != 11  || (dotImage15.tag != 15 && dotImage7.tag != 7 && dotImage9.tag != 9) ) {
+        
+        [self InvalidMove];
+        
+    }
+    else {
+        
+        NSLog(@"OK");
+    
+    }
+    
+    
+  /*  if (dotImage1.tag == 1 && dotImage2.tag == 2) {
+        
+        [HorizontalLine1 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+        
+    }
+    else if (dotImage2 && dotImage3){
+        
+        [HorizontalLine2 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    else if (dotImage3 && dotImage4){
+        
+        [HorizontalLine3 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    else if (dotImage4 && dotImage5){
+        
+        [HorizontalLine4 setImage:[UIImage imageNamed:@"red straight line 18Aug.png"]];
+    }
+    else if (dotImage5 && dotImage10){
+        
+        [VerticalLine3 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    else if (dotImage10 && dotImage15){
+        
+        [VerticalLine4 setImage:[UIImage imageNamed:@"red vertical line 2 18Aug.png"]];
+    }
+    else if (dotImage15 && dotImage8){
+        
+        [DiagonalLine1 setImage:[UIImage imageNamed:@"right red diagonal line 18Aug.png"]];
+    }
+    else if (dotImage11 && dotImage8){
+        
+        [DiagonalLine2 setImage:[UIImage imageNamed:@"left red diagonal line 18Aug.png"]];
+    }
+     */
 
   //  drawImage.image = UIGraphicsGetImageFromCurrentImageContext();
    
-    if (dotImage1.tag != 1 || dotImage2.tag != 2 || dotImage3.tag != 3 || dotImage4.tag != 4 || dotImage5.tag != 5 || dotImage6.tag != 6 || dotImage7.tag != 7 || dotImage8.tag == 8 || dotImage9.tag != 9 || dotImage10.tag != 10 || dotImage11.tag == 11 || dotImage12.tag != 12 || dotImage13.tag != 13 || dotImage14.tag != 14 || dotImage15.tag != 15 ) {
+/*    if (dotImage1.tag != 1 || dotImage2.tag != 2 || dotImage3.tag != 3 || dotImage4.tag != 4 || dotImage5.tag != 5 || dotImage6.tag != 6 || dotImage7.tag != 7 || dotImage8.tag == 8 || dotImage9.tag != 9 || dotImage10.tag != 10 || dotImage11.tag == 11 || dotImage12.tag != 12 || dotImage13.tag != 13 || dotImage14.tag != 14 || dotImage15.tag != 15 ) {
         
         [self InvalidMove];
         lastPoint = CGPointMake(0, 0);
@@ -197,7 +463,7 @@
                                         repeats:NO];
         
         NSLog(@"if is no");
-    }
+    }       */
 }
 
 
@@ -206,7 +472,7 @@
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"WARNING" message:@"INVALID MOVE" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:nil, nil];
     [alert show];
     drawImage.image = nil;
-    
+    [self ResetButton:self];
     [self.moviePlayer stop];
     
 }
@@ -256,5 +522,68 @@
     
 }
 
+
+- (IBAction)ResetButton:(id)sender {
+    
+    HorizontalLine1.image = nil;
+    HorizontalLine2.image = nil;
+    HorizontalLine3.image = nil;
+    HorizontalLine4.image = nil;
+    HorizontalLine5.image = nil;
+    HorizontalLine6.image = nil;
+    HorizontalLine7.image = nil;
+    HorizontalLine8.image = nil;
+    HorizontalLine9.image = nil;
+    HorizontalLine10.image = nil;
+    HorizontalLine11.image = nil;
+    HorizontalLine12.image = nil;
+    
+    VerticalLine1.image = nil;
+    VerticalLine2.image = nil;
+    VerticalLine3.image = nil;
+    VerticalLine4.image = nil;
+    VerticalLine5.image = nil;
+    VerticalLine6.image = nil;
+    VerticalLine7.image = nil;
+    VerticalLine8.image = nil;
+    VerticalLine9.image = nil;
+    VerticalLine10.image = nil;
+    
+    DiagonalLine1.image = nil;
+    DiagonalLine2.image = nil;
+    
+    
+    dotImage1.tag = 0;
+    dotImage2.tag = 0;
+    dotImage3.tag = 0;
+    dotImage4.tag = 0;
+    dotImage5.tag = 0;
+    dotImage6.tag = 0;
+    dotImage7.tag = 0;
+    dotImage8.tag = 0;
+    dotImage9.tag = 0;
+    dotImage10.tag = 0;
+    dotImage11.tag = 0;
+    dotImage12.tag = 0;
+    dotImage13.tag = 0;
+    dotImage14.tag = 0;
+    dotImage15.tag = 0;
+    
+    DotImage1 = NO;
+    DotImage2 = NO;
+    DotImage3 = NO;
+    DotImage4 = NO;
+    DotImage5 = NO;
+    DotImage6 = NO;
+    DotImage7 = NO;
+    DotImage8 = NO;
+    DotImage9 = NO;
+    DotImage10 = NO;
+    DotImage11 = NO;
+    DotImage12 = NO;
+    DotImage13 = NO;
+    DotImage14 = NO;
+    DotImage15 = NO;
+}
 
 @end
